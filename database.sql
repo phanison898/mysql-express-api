6c8eae8aa82e956591bcaf2feca4be4eff27d053
phanison898 <phanison898@gmail.com>
2023-01-02 21:32:29 +0530

    added sql schema to create database in mysql

---
+++ database.sql
@@ -0,0 +1,26 @@
+-- Create database
+CREATE DATABASE memories;
+
+-- Use the database
+USE memories;
+
+-- Create table-1 for users
+CREATE TABLE users(
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    mobile VARCHAR(255),
+    profile_pic VARCHAR(255),
+    PRIMARY KEY (id)
+);
+
+-- Create table-2 for posts
+CREATE TABLE posts(
+	id INT NOT NULL AUTO_INCREMENT,
+    description VARCHAR(255) NOT NULL,
+    media VARCHAR(255) NOT NULL,
+    user_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
